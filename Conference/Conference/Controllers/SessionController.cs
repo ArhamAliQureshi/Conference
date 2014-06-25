d7601b0eecc8d1e0ef4a061926958d99164f7bda
arhamaliqureshi <arhamaliqureshi@yahoo.com>
2014-06-25 21:16:21 +0500

    initial commit

---
+++ Conference/Conference/Controllers/SessionController.cs
@@ -0,0 +1,55 @@
+﻿using Conference.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Conference.Controllers
+{
+    public class SessionController : Controller
+    {
+        //
+        // GET: /Session/
+
+        public ActionResult Index()
+        {
+            ConferenceContext Context = new ConferenceContext();
+            List<Session> Sessions = Context.Sessions.ToList();
+            return View("Index", Sessions);
+        }
+
+        // GET: Session/Create
+        [HttpGet()]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // PSOT: Session/Create
+        [HttpPost()]
+        public ActionResult Create(Session Session)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(Session);
+            }
+            try
+            {
+                ConferenceContext Context = new ConferenceContext();
+                Context.Sessions.Add(Session);
+                Context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Error", ex.Message);
+                return View(Session);
+            }
+            
+
+            TempData["Message"] = "Created" + Session.Title;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Conference/Conference/Models/ConferenceContextInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Conference.Models
+{
+    public class ConferenceContextInitializer : DropCreateDatabaseAlways<ConferenceContext>
+    {
+        protected override void Seed(ConferenceContext Context)
+        {
+            Context.Sessions.Add(
+                new Session()
+                {
+                    Title = "I want Spaghetti",
+                    Abstract = "This is life and times of a spaghetti lover",
+                    Speaker = Context.Speakers.Add(
+                    new Speaker()
+                    {
+                        Name= "Arham Ali Qureshi",
+                        EmailAddress= "arhamali20022002@gmail.com",
+                    })
+                });
+            Context.SaveChanges();
+        }
+    }
+}
